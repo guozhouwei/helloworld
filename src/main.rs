561a20a8452ca417afa493985e36a7066bf0eb8a
Devin <guozwhero@126.com>
2023-07-02 13:20:40 +0800

    666
    666

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::string;
+
 fn main() {
+    /*
+     * （1）添加一个一层子模块，循环打印从’a’~’Z’ 之间的所有字符
+     * （2）添加一个二层子模块，循环打印从’A’~’z’ 之间的所有字符
+     * （3）使用Cargo编译运行此工程
+     * （需要自行发现其中的细节，一个考点是：ascii码字符的顺序）
+     */
     println!("打印 Z~a:");
     for ascii1 in ('Z'..='a').rev() {
         print!("{ascii1} ");
@@ -9,4 +17,48 @@ fn main() {
     for ascii2 in 'A'..='z' {
         print!("{ascii2}");
     }
+
+}
+
+#[test]
+/*
+* 思考题：
+* 如何将字符串转换为字符数组?
+* String -> [char] 或 Vec<char>
+*/
+fn test_string_convert_to_() {    
+    let s = String::from("initial contents");
+    let hello1 = String::from("عليكم السلام");
+    let hello2 = String::from("Dobrý den");
+    let hello3 = String::from("Hello");
+    let hello4 = String::from("こんにちは");
+    let hello5 = String::from("안녕하세요");
+    let hello6 = String::from("你好");
+
+    let mut fix = String::from("$$$$");
+    fix.push_str(&s);
+    fix.push_str(&hello1);
+    fix.push_str(&hello2);
+    fix.push_str(&hello3);
+    fix.push_str(&hello4);
+    fix.push_str(&hello5);
+    fix.push_str(&hello6);
+    //
+    strConvertVecAndArray(&fix);
+
+
+}
+
+fn strConvertVecAndArray(para: &str) {
+
+    //1. Vec<char>
+    let charVec: Vec<char> = para.chars().collect::<Vec<_>>();
+    println!("Vec<char>:{:?}", charVec);
+    
+
+    println!("");
+
+    //2. [char]
+    let chars = para.chars();
+    println!("[charArray]:{:?}", chars);
 }
